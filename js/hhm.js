22716ff0bc4cbe16aca6dac7f3eda17b29ab8e25
Lee Crossley <leee@hotmail.co.uk>
2013-09-09 16:36:10 +0100

    Working timelapse

--- js/hhm.js
+++ js/hhm.js
@@ -1,6 +1,6 @@
 var hhm = (function () {
     "use strict";
-    var hhm = {}, chart, year;
+    var hhm = {}, chart, year, current = 0, dataset = [], isPlaying;
     
     var getDataForYear = lambda.partition(function (item) {
         return item.year === year;
@@ -10,18 +10,56 @@ var hhm = (function () {
         return [item.country, parseFloat(item.value)];
     });
     
+    var nextYear = function (initial) {
+        if (!isPlaying && !initial) {
+            return;
+        }
+        chart.draw(google.visualization.arrayToDataTable(dataset[current]));
+        $(".year").text(current + 2000);
+        $(".progress-bar").width(current * 10 + "%");
+        if (current === 10) {
+            current = 0;
+        } else {
+            current++;
+        }
+        if (!initial) {
+            setTimeout(nextYear, 1500);
+        }
+    };
+    
+    var configurePlay = function () {
+        $(".play").on("click", function() {
+           if (!isPlaying) {
+               isPlaying = true;
+               nextYear();
+               $(this).removeClass("btn-success");
+               $(this).addClass("btn-warning");
+               $(this).text("Stop");
+           } else {
+               isPlaying = false;
+               $(this).removeClass("btn-warning");
+               $(this).addClass("btn-success");
+               $(this).text("Play");
+           }
+        });
+    };
+    
     var getData = function () {
         $.getJSON("/data/recipient-commitment.json", function(data) {
-            var dataForYear = getDataForYear(data)[0];
-            var mapData = getMapData(dataForYear);
-            mapData.unshift(["Country", "Commitment"]);
-            chart.draw(google.visualization.arrayToDataTable(mapData));
+            for (var i = 2000; i <= 2010; i++) {
+                year = i.toString();
+                var dataForYear = getDataForYear(data)[0];
+                var mapData = getMapData(dataForYear);
+                mapData.unshift(["Country", "Commitment"]);
+                dataset.push(mapData);
+            }
+            nextYear(true);
+            configurePlay();
         });
     };
     
     var initMap = function () {
         chart = new google.visualization.GeoChart(document.getElementById("heatmap"));
-        year = "2000";
         getData();
     };
     
